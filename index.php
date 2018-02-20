1bfa0a65c43519fc60a89f3999795506b2e5cfdc
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-02-20 09:16:52 -0700

    created index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script type="text/javascript" src="javascript.js></script>
+		</head>
+
+		<title>Basic HTML Page</title>
+	</head>
+	<body>
+			<div id="baconIpsumOutput">
+			</div>
+		<h1>Hello, World! :D</h1>
+
+
+	</body>
+</html>
